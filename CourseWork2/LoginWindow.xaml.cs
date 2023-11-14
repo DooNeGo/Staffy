04cece7ce3320df50ed36062bfb454d48c75dd28
DooNeGo <matveyprostomac@gmail.com>
2023-11-14 09:07:20 +0300

    Добавьте файлы проекта.

---
+++ CourseWork2/LoginWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CourseWork2
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginForm.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CourseWork2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Messaging;
+using Microsoft.Data.SqlClient;
+using System.Windows;
+
+namespace CourseWork2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "SERVER=localhost;DATABASE=MyDB;UID=root;PASSWORD=;";
+            SqlConnection connection = new(connectionString);
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("123");
+        }
+    }
+}
