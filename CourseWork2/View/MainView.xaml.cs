1440208f7cf1fdf1ed8a8764717daabd8db8916b
DooNeGo <matveyprostomac@gmail.com>
2023-12-23 01:11:12 +0300

    Add control buttons to main view, add colors for maximize button, and make the edges rounding smaller

--- CourseWork2/View/MainView.xaml.cs
+++ CourseWork2/View/MainView.xaml.cs
@@ -2,12 +2,10 @@
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+using Microsoft.Win32;
 
 namespace CourseWork2.View;
 
-/// <summary>
-///     Логика взаимодействия для MainView.xaml
-/// </summary>
 public partial class MainView
 {
     private readonly WindowInteropHelper _helper;
@@ -15,21 +13,50 @@ public partial class MainView
     public MainView()
     {
         InitializeComponent();
-        _helper = new WindowInteropHelper(this);
+
+        _helper   = new WindowInteropHelper(this);
+        MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        MaxWidth  = SystemParameters.MaximizedPrimaryScreenWidth;
+        
+        SystemEvents.DisplaySettingsChanging += OnDisplaySettingsChanging;
+        Closed += OnClosed;
     }
-    
-        [DllImport("user32.dll")]
+
+    private void OnClosed(object? sender, EventArgs e)
+    {
+        SystemEvents.DisplaySettingsChanging -= OnDisplaySettingsChanging;
+    }
+
+    private void OnDisplaySettingsChanging(object? sender, EventArgs e)
+    {
+        MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        MaxWidth  = SystemParameters.MaximizedPrimaryScreenWidth;
+    }
+
 #pragma warning disable SYSLIB1054
-        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+    [DllImport("user32.dll")]
+    private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
 #pragma warning restore SYSLIB1054
 
-private void ControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    private void ControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
         SendMessage(_helper.Handle, 161, 2, 0);
     }
 
-    private void ControlBar_OnMouseEnter(object sender, MouseEventArgs e)
+    private void CloseButton_OnClick(object sender, RoutedEventArgs e)
     {
-        MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        Application.Current.Shutdown();
+    }
+
+    private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState is WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
     }
 }
