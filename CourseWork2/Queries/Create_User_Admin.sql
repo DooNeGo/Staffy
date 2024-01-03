c281b8f5fb460828be7fe29e5c0fbd4d672af46e
DooNeGo <matveyprostomac@gmail.com>
2024-01-03 16:39:11 +0300

    Change users privileges

--- CourseWork2/Queries/Create_User_Admin.sql
+++ CourseWork2/Queries/Create_User_Admin.sql
@@ -1,15 +1,18 @@
 ﻿USE courseworkdb;
 
-CREATE USER 'user'@'localhost' IDENTIFIED BY '1111';
+DROP USER 'user'@'localhost';
+DROP USER 'admin'@'localhost';
+
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY '1111';
 GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.departments TO 'user'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.workers TO 'user'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.positions TO 'user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.accepted_workers TO 'user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.fired_workers TO 'user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.retired_workers TO 'user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON courseworkdb.users TO 'user'@'localhost';
+GRANT SELECT ON courseworkdb.accepted_workers TO 'user'@'localhost';
+GRANT SELECT ON courseworkdb.fired_workers TO 'user'@'localhost';
+GRANT SELECT ON courseworkdb.retired_workers TO 'user'@'localhost';
+GRANT SELECT ON courseworkdb.users TO 'user'@'localhost';
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY '2222';
+CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY '2222';
 GRANT ALL PRIVILEGES ON courseworkdb.* TO 'admin'@'localhost';
 
-FLUSH PRIVILEGES 
+FLUSH PRIVILEGES;
